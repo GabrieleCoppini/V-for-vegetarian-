6cfff5091452afe65c88596e4516f3e0e81a4d96
GabrieleCoppini <gabrielecoppini@outlook.it>
2025-04-19 19:32:01 +0200

    6

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Pages from "./pages/Pages";
 import Search from "./components/Search";
+import Home from "./pages/Home";
 
 
 function App() {
   return <div className="App">
     <BrowserRouter>
        <Search />
+       <Home  />
        <Pages />
     </BrowserRouter>
   </div>;
